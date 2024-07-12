b0c491ca018c747180878c622ef1ac3047791aa9
bikky <bikkydahal@gmail.com>
2024-07-12 14:50:44 +0545

    Completed simple challenge with the concept of refs, Imperativehandle, portals

--- Refs and Portal/src/components/Player.jsx
+++ Refs and Portal/src/components/Player.jsx
@@ -1,25 +1,18 @@
 import { useState, useRef } from 'react';
 
 export default function Player() {
-  const playerName = useRef()
+  const playerName = useRef();
 
-  const [name, setName] = useState('Noob');
-  // const [submitted, setSubmitted] = useState(false);
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
-
-  // function handleChange(e) {
-  //   setSubmitted(false);
-  //   setName(e.target.value);
-  // }
-
-  function handleClick(){
-    setName(playerName.current.value);
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
   }
-
 
   return (
     <section id="player">
-      <h2>Welcome {name ?? 'Noob'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
         <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
