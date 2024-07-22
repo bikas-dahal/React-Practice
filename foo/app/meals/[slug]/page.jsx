478604135a06e2fe92d976725bec16a528c0077a
bikky <bikkydahal@gmail.com>
2024-07-22 08:32:29 +0545

    Sqlite db added

--- foo/app/meals/[slug]/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function ({params}) {
-    return (
-        <div>
-        <h1>Meals</h1>
-        <p>Discover delicious meals shared by our community.</p>
-        {params.slug}
-        </div>
-    );
-}
--- foo/app/meals/page.js
+++ foo/app/meals/page.js
@@ -1,8 +1,36 @@
-export default function() {
-    return (
-        <div>
-        <h1>Meals</h1>
-        <p>Discover delicious meals shared by our community.</p>
-        </div>
-    );
+import { Suspense } from 'react';
+import Link from 'next/link';
+
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
 }
