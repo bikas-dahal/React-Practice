4141425993dc00d965c682fd0ee3379e0910f8b5
Bikas Dahal <bikkydahal@gmail.com>
2024-09-10 20:54:18 +0545

    Minor index changes

--- Port/vite-react/src/sections/Hero.jsx
+++ Port/vite-react/src/sections/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
     <section className="min-h-screen w-full flex flex-col relative" id="home">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
-          Hi, I am Adrian <span className="waving-hand">👋</span>
+          Hi, I am Bikas <span className="waving-hand">👋</span>
         </p>
         <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
       </div>
