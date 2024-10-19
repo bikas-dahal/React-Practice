e8a0a5523bf6a3f291b41c19c7ad5a463ec65a40
Bikas Dahal <bikkydahal@gmail.com>
2024-10-19 12:34:04 +0545

    little message

--- NextJS/salak/src/app/workspace/[workspaceId]/layout.tsx
+++ NextJS/salak/src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
 import { Sidebar } from "./sidebar";
 import ToolBar from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface WorkspaceIdLayoutProps {
     children: React.ReactNode
@@ -13,7 +15,16 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
             <ToolBar />
             <div className="flex h-[calc(100vh-40px)]">
                 <Sidebar />
-                {children}
+                <ResizablePanelGroup direction="horizontal" autoSaveId={'salak-workspace-layout'}>
+                    <ResizablePanel defaultSize={20} minSize={11} className="bg-[#5e2c5F] ">
+                        <WorkspaceSidebar />
+                    </ResizablePanel>
+                    <ResizableHandle withHandle />
+                    <ResizablePanel minSize={20}>
+                        {children}
+                        Tomorrow is my license exam, will contribute in better way after tommorrow ok,
+                    </ResizablePanel>
+                </ResizablePanelGroup>
 
             </div>
         </div>
---
+++ NextJS/salak/src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -0,0 +1,7 @@
+export const WorkspaceSidebar = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
